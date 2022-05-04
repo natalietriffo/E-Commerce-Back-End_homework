f2603f82c364f8c997eb1194ff92c66a4f4fc6f8
Natalie Triffo <nattriffo@gmail.com>
2022-05-04 18:28:05 -0500

    creating tag model

--- Develop/models/Tag.js
+++ Develop/models/Tag.js
@@ -7,7 +7,22 @@ class Tag extends Model {}
 Tag.init(
   {
     // define columns
+    id: {
+
+      type: DataTypes.INTEGER, 
+      allowNull: false,
+
+      autoIncrement: true, 
+      primaryKey: true
+
+    },
+    tag_name: {
+
+      type:DataTypes.STRING
+
+    }
   },
+
   {
     sequelize,
     timestamps: false,
