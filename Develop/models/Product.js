e5e3f4734ccae4a549a60a5feb3c5cbd7c28994d
Natalie Triffo <nattriffo@gmail.com>
2022-05-04 18:24:18 -0500

    creating the table for product

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -10,7 +10,49 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id: {
+
+      type: DataTypes.INTEGER, 
+      allowNull: false,
+
+      autoIncrement: true, 
+      primaryKey: true
+
+    },
+    product_name: {
+
+      type: DataTypes.STRING,
+      allowNull: false,
+
+    },
+    category_id: {
+
+      type: DataTypes.INTEGER, 
+      references: {
+        model: "category",
+        key: "id",
+
+      }
+
+    },
+    price: {
+      type: DataTypes.DECIMAL(2),
+      allowNull: false,
+      validate: {
+        isDecimal:true
+      }
+    },
+    stock:{
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true
+      }
+
+    }
   },
+
   {
     sequelize,
     timestamps: false,
