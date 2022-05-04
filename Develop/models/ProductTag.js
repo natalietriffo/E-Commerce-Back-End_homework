d3d2728af44c564da3d61da64ffd08518981058b
Natalie Triffo <nattriffo@gmail.com>
2022-05-04 18:32:33 -0500

    product and tag models

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -7,7 +7,33 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
-    
+    id: {
+
+      type: DataTypes.INTEGER, 
+      allowNull: false,
+
+      autoIncrement: true, 
+      primaryKey: true
+
+    },
+
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "tag",
+        key: "id"
+
+      }
+    },
+    product_id:{
+
+      type: DataTypes.INTEGER,
+      references: {
+        model: "prodect",
+        key: "id"
+      }
+
+    }
   },
   {
     sequelize,
